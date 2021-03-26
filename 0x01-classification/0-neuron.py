3ca2737743296634c137b54cd270ce587618d35e
karimaAITHABA <78323027+karima1999@users.noreply.github.com>
2021-03-26 22:50:36 +0100

    Update 0-neuron.py

--- 0x01-classification/0-neuron.py
+++ 0x01-classification/0-neuron.py
@@ -6,6 +6,6 @@ class Neuron:
             raise TypeError('nx must be an integer')
         if (nx<1):
             raise ValueError('nx must be a positive integer')
-        self.W = np.random.normal()
+        self.W = np.random.randn(1,2)
         self.b = 0
         self.A = 0
